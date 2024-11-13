9af81084a2b676c24b4cd4d86fbde152559c39f1
ZPU <kuromimeetsvenom@gmail.com>
2024-11-13 18:06:54 +0700

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -4,5 +4,5 @@ getgenv().Games = {
     [7449423635] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- B1
     [3351674303] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/8972621456ab46418f51c66349616f22.lua", -- D
     [18901165922] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/c12281ed5e3842108c859ffbd9c1bb961.lua", -- P
-    [16732694052] = "https://raw.githubusercontent.com/Spectrum-Trash/Bin/main/995ccfcb48bb4adaa66b371cfcc37ae4.lua", -- F
+    [16732694052] = "https://raw.githubusercontent.com/Spectrum-Trash/Bin/main/ff686405aff0429288322ca3260984de.lua", -- F
   }
