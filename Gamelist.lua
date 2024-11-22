0288e7aa55bc1c2c99998e3b6cec264bf6d0a453
ZPU <kuromimeetsvenom@gmail.com>
2024-11-22 21:47:38 +0700

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,8 +1,8 @@
 getgenv().Games = {
-    [2753915549] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- B3
-    [4442272183] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- B2
-    [7449423635] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- B1
-    [3351674303] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/8972621456ab46418f51c66349616f22.lua", -- D
-    [18901165922] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/c12281ed5e3842108c859ffbd9c1bb961.lua", -- P
-    [16732694052] = "https://raw.githubusercontent.com/Spectrum-Trash/Bin/main/ff686405aff0429288322ca3260984de.lua", -- F
+    [2753915549] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruits Third Sea
+    [4442272183] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruits Second Sea
+    [7449423635] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruits First Sea
+    [3351674303] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/8972621456ab46418f51c66349616f22.lua", -- Driving empire
+    [18901165922] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/c12281ed5e3842108c859ffbd9c1bb961.lua", -- Pets Go
+    [16732694052] = "https://api.luarmor.net/files/v3/loaders/6eb027c55a4f31369d8213fa989514ad.lua", -- Fisch
   }
