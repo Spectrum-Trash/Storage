913d0df0c014899bfba6a0bfe80fcf0ab0f3353e
ZPU <kuromimeetsvenom@gmail.com>
2025-01-13 00:35:12 +0700

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -6,4 +6,6 @@ getgenv().Games = {
   [18901165922] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/c12281ed5e3842108c859ffbd9c1bb961.lua", -- Pets Go
   [16732694052] = "https://api.luarmor.net/files/v3/loaders/6eb027c55a4f31369d8213fa989514ad.lua", -- Fisch
   [8304191830] = "https://api.luarmor.net/files/v3/loaders/815a58c05dc1e770d9d1506d24ac006f.lua", -- Anime Adventures
+  [10450270085] = "https://api.luarmor.net/files/v3/loaders/732a5fbd9e4bca3f28df5bc2293aa5ab.lua", -- Jujutsu Infinite
+  [118693886221846] = "https://api.luarmor.net/files/v3/loaders/732a5fbd9e4bca3f28df5bc2293aa5ab.lua", -- Jujutsu Infinite
 }
