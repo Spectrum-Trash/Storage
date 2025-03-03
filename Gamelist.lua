5a160fa63c949a918f2298c9f5e697aadee6dbbc
ZPU <kuromimeetsvenom@gmail.com>
2025-03-04 01:50:51 +0700

    Update Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -8,4 +8,9 @@ getgenv().Games = {
   [8304191830] = "https://api.luarmor.net/files/v3/loaders/815a58c05dc1e770d9d1506d24ac006f.lua", -- Anime Adventures
   [10450270085] = "https://api.luarmor.net/files/v3/loaders/732a5fbd9e4bca3f28df5bc2293aa5ab.lua", -- Jujutsu Infinite
   [118693886221846] = "https://api.luarmor.net/files/v3/loaders/732a5fbd9e4bca3f28df5bc2293aa5ab.lua", -- Jujutsu Infinite
+  [18668065416] = "https://api.luarmor.net/files/v3/loaders/fbc854090940d543550e6396a0d7c947.lua", -- Blue Lock: Rivals // Main
+  [115110570222234] = "https://api.luarmor.net/files/v3/loaders/fbc854090940d543550e6396a0d7c947.lua", -- Blue Lock: Rivals // Match
+  [99999183305180] = "https://api.luarmor.net/files/v3/loaders/fbc854090940d543550e6396a0d7c947.lua", -- Blue Lock: Rivals // Pro Servers
+  [92517437168342] = "https://api.luarmor.net/files/v3/loaders/fbc854090940d543550e6396a0d7c947.lua", -- Blue Lock: Rivals // Customs
+  [82577947590704] = "https://api.luarmor.net/files/v3/loaders/fbc854090940d543550e6396a0d7c947.lua", -- Blue Lock: Rivals // Intro
 }
