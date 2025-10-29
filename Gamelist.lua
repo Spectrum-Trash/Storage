b9c32004e53324368424355988bdff9d24d1b009
ZPU <kuromimeetsvenom@gmail.com>
2025-10-29 09:04:17 +0700

    Update game URLs in Gamelist.lua

--- Gamelist.lua
+++ Gamelist.lua
@@ -1,10 +1,4 @@
 getgenv().Games = {
-  [2753915549] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruits Third Sea
-  [4442272183] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruits Second Sea
-  [7449423635] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/SpectrumX.lua", -- Blox Fruits First Sea
-  [3351674303] = "https://api.luarmor.net/files/v3/loaders/3ccad5bf2746c79635e67e1dfb023ad1.lua", -- Driving empire
-  [18901165922] = "https://raw.githubusercontent.com/xZPUHigh/Project-Spectrum/main/c12281ed5e3842108c859ffbd9c1bb961.lua", -- Pets Go
-  [16732694052] = "https://api.luarmor.net/files/v3/loaders/6eb027c55a4f31369d8213fa989514ad.lua", -- Fisch
   [8304191830] = "https://api.luarmor.net/files/v3/loaders/815a58c05dc1e770d9d1506d24ac006f.lua", -- Anime Adventures
   [10450270085] = "https://api.luarmor.net/files/v3/loaders/732a5fbd9e4bca3f28df5bc2293aa5ab.lua", -- Jujutsu Infinite
   [118693886221846] = "https://api.luarmor.net/files/v3/loaders/732a5fbd9e4bca3f28df5bc2293aa5ab.lua", -- Jujutsu Infinite
@@ -14,5 +8,7 @@ getgenv().Games = {
   [92517437168342] = "https://api.luarmor.net/files/v3/loaders/fbc854090940d543550e6396a0d7c947.lua", -- Blue Lock: Rivals // Customs
   [82577947590704] = "https://api.luarmor.net/files/v3/loaders/fbc854090940d543550e6396a0d7c947.lua", -- Blue Lock: Rivals // Intro
   [72829404259339] = "https://api.luarmor.net/files/v3/loaders/d9b4281dc4937a8409fd4069d25c6885.lua", -- Anime Rangers X
-  [126884695634066] = "https://api.luarmor.net/files/v3/loaders/60985c952c056c27e9a691f6e60552d1.lua", -- Grow a Garden
+  [3351674303] = "https://api.luarmor.net/files/v3/loaders/3dd8029e73d52fb980e0ec5fb6aeda2a.lua", -- Driving empire
+  [16732694052] = "https://api.luarmor.net/files/v3/loaders/95f7791a6fd1d4e10028ee42363ddaab.lua", -- Fisch
+  [131716211654599] = "https://api.luarmor.net/files/v3/loaders/95f7791a6fd1d4e10028ee42363ddaab.lua", -- Fisch // New Players Servers
 }
