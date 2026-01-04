9d67c948e7d32ba9082bbe9c437af4447a665dff
ZPU <kuromimeetsvenom@gmail.com>
2026-01-05 02:11:43 +0700

    Update Sober.lua

--- Sober.lua
+++ Sober.lua
@@ -1,6 +1,27 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Spectrum-Trash/Storage/main/Gamelist.lua"))()
-for PlaceID, Execute in pairs(Games) do
-    if PlaceID == game.PlaceId then
-        loadstring(game:HttpGet(Execute))()
+
+local Games = getgenv().Games
+local supported = false
+
+local function spectrum_console(message)
+    print("================================")
+    print("[SPECTRUM ASSISTANT]: " .. message)
+    print("================================")
+end
+
+if type(Games) == "table" then
+    for PlaceID, Execute in pairs(Games) do
+        if PlaceID == game.PlaceId then
+            supported = true
+            spectrum_console("Game Supported! Loading Script...")
+            loadstring(game:HttpGet(Execute))()
+            break
+        end
     end
+end
+
+if not supported then
+    spectrum_console("Game Not Supported!")
+    task.wait(0.5)
+    game.Players.LocalPlayer:Kick("[SPECTRUM ASSISTANT]: Game Not Supported ❌\nhttps://discord.gg/C3MpUNwsDU")
 end
